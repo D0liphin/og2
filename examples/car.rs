ee6c1c902b3c48c825f99795676be2f3976f1d75
Oli <oli_iliffe@hotmail.com>
2021-09-08 17:55:32 +0100

    fixed bugs

--- examples/car.rs
+++ examples/car.rs
@@ -1,127 +1,86 @@
-// use lazy_static::lazy_static;
-// use oge::{Oge, Script};
-// use rand::Rng;
-// use std::{
-//     f32::consts::PI,
-//     path::PathBuf,
-//     time::{Duration, Instant},
-// };
+use oge::{Oge, Script, Vector2};
 
-// struct Car {
-//     sprite: oge::Sprite,
-//     position: oge::Vector2,
-//     velocity: oge::Vector2,
-//     speed: f32,
-//     torque: f32,
-//     angle: f32,
-// }
+struct WindowHandler;
 
-// impl Script for Car {
-//     fn start(oge: &mut Oge) -> Self {
-//         Car {
-//             sprite: oge::Sprite::new(oge::SpriteConfiguration {
-//                 label: Some("Car"),
-//                 mesh: oge::SpriteMesh::new_rectangle(64.0, 64.0),
-//                 texture: oge
-//                     .create_texture(&oge::TextureConfiguration {
-//                         source: oge::TextureSource::Bytes(include_bytes!("./car.png")),
-//                         filter_mode: oge::FilterMode::Point,
-//                         ..Default::default()
-//                     })
-//                     .unwrap(),
-//             }),
-//             position: oge::Vector2::new(0.0, 0.0),
-//             velocity: oge::Vector2::new(0.0, 0.0),
-//             speed: 0.0,
-//             angle: 0.0,
-//             torque: 0.0,
-//         }
-//     }
+impl Script for WindowHandler {
+    fn start(_: &mut Oge) -> oge::Result<Self> {
+        Ok(Self)
+    }
 
-//     fn update(&mut self, oge: &mut Oge) {
-//         if oge.window_has_resized() {
-//             let window_dimensions = oge.window_dimensions();
-//             let (x, y) = (
-//                 window_dimensions.width as f32 * 0.5,
-//                 window_dimensions.height as f32 * 0.5,
-//             );
-//             oge.set_window_bounds(oge::Bounds {
-//                 bottom_left: oge::Vector2::new(-x, -y),
-//                 top_right: oge::Vector2::new(x, y),
-//             });
-//         }
+    fn update(&mut self, oge: &mut Oge) {
+        if oge.window_has_resized() {
+            let top_right = oge.window_dimensions().as_vector2().scale(0.5);
+            let bottom_left = top_right.scale(-1.);
+            oge.set_window_bounds(oge::Bounds {
+                bottom_left,
+                top_right,
+            });
+        }
+    }
+}
 
-//         let cursor_position = oge.get_real_position(&oge.cursor_position());
-//         let direction_vector = oge::Vector2::new_euclidean(self.angle, 1.0);
-//         let car_cursor_angle =
-//             oge::Vector2::angle_between(&direction_vector, &(cursor_position.sub(&self.position)));
+struct Car {
+    car_sprite: oge::Sprite,
+    dust_particle_sprite: oge::Sprite,
+    position: Vector2,
+    velocity: Vector2,
+    direction: f32,
+    torque: f32,
+}
 
-//         if self.speed > 5.0 {
-//             if car_cursor_angle.is_sign_positive() {
-//                 self.torque += oge.delta_time() * 0.2 * car_cursor_angle.powi(2);
-//                 if self.torque > 0.15 {
-//                     self.torque = 0.15;
-//                 }
-//             } else {
-//                 self.torque -= oge.delta_time() * 0.2 * car_cursor_angle.powi(2);
-//                 if self.torque < -0.15 {
-//                     self.torque = -0.15;
-//                 }
-//             }
-//         }
-//         self.torque *= oge.delta_time() * -5.0 * self.torque + 0.95;
-//         self.angle += self.torque
-//             * if self.speed < 200.0 {
-//                 0.005 * self.speed
-//             } else {
-//                 1.0
-//             };
+impl Script for Car {
+    fn start(oge: &mut Oge) -> oge::Result<Self> {
+        let car_sprite = oge.create_sprite(oge::SpriteConfiguration {
+            label: Some("Car Sprite"),
+            mesh: oge::SpriteMesh::new_rectangle(64., 64.),
+            default_texture: oge.create_texture(&oge::TextureConfiguration {
+                source: oge::TextureSource::Bytes(include_bytes!("./car.png")),
+                filter_mode: oge::FilterMode::Point,
+                ..Default::default()
+            })?,
+            z_index: oge::ZIndex::Specific(1),
+            opacity: 1.,
+            texture_projection_method: oge::TextureProjectionMethod::ScaleToFit,
+        })?;
 
-//         if oge.get_mouse_button_down(oge::MouseButtonCode::Left) {
-//             let distance =
-//                 (self.speed + 20.0) * 0.001 * self.position.distance_to(&cursor_position);
-//             let distance = if distance > 20.0 { 20.0 } else { distance };
-//             self.speed += distance;
+        let dust_particle_sprite = oge.create_sprite(oge::SpriteConfiguration {
+            label: Some("Dust Particle Sprite"),
+            mesh: oge::SpriteMesh::new_elipse(4., 4., 8),
+            default_texture: oge.create_texture(&oge::TextureConfiguration::color(
+                oge::Color::new(0.4, 0.4, 0.4, 1.),
+            ))?,
+            z_index: oge::ZIndex::Specific(0),
+            opacity: 1.,
+            texture_projection_method: oge::TextureProjectionMethod::SingleColor,
+        })?;
 
-//             let max_speed = distance * 10.0;
+        let velocity = Vector2::ZERO;
+        let direction = 0.;
+        let torque = 0.;
 
-//             if self.speed > max_speed {
-//                 self.speed -= distance;
-//                 if self.speed < max_speed {
-//                     self.speed = max_speed;
-//                 }
-//             }
-//         } else {
-//             self.speed -= 300.0 * self.torque.abs();
-//             if self.speed < 0.0 {
-//                 self.speed = 0.0;
-//             }
-//         }
+        Ok(Self {
+            car_sprite,
+            dust_particle_sprite,
+            velocity,
+            direction,
+            torque,
+        })
+    }
 
-//         self.velocity.scale_assign(0.95);
-//         self.velocity
-//             .add_assign(&direction_vector.scale(self.speed));
-//         let velocity_magnitude = self.velocity.magnitude();
-//         if velocity_magnitude > 800.0 {
-//             self.velocity.scale_assign(800.0 / velocity_magnitude);
-//         }
+    fn update(&mut self, oge: &mut Oge) {
+        let mut acceleration = Vector2::new_euclidean(self.direction, 1.);
+        if oge.get_key_down(oge::KeyCode::W) {
+            acceleration.scale_assign(50.);
+        }
+        
+    }
 
-//         self.position
-//             .add_assign(&self.velocity.scale(oge.delta_time()));
+    fn render(&mut self, oge: &mut Oge) {
+        self.car_sprite.set_position(self.position);
+        oge.draw_once(&self.car_sprite);
+    }
+}
 
-//         self.sprite
-//             .set_transformation(oge::Matrix2::rotation(self.angle));
-//         self.sprite.set_position(self.position);
-//     }
-
-//     fn render(&mut self, oge: &mut Oge) {
-//         oge.draw_sprites(std::iter::once(&self.sprite));
-//     }
-// }
-
-// fn main() {
-//     oge::main_loop::start([Car::load_script()]).unwrap();
-// }
-
-
-fn main() {}
+fn main() -> oge::Result<()> {
+    oge::main_loop::start([Car::load_script(), WindowHandler::load_script()])
+}
